aa1f4c9dccd1b5e5ea21c37bef3b9d8d916428f9
jamiahuswalton <jamiahuswalton@gmail.com>
2024-01-20 01:00:42 -0600

    Created Shopping List Item Composable
    Created this composable to display each shopping list item

--- app/src/main/java/com/jamiahus/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/jamiahus/myshoppinglistapp/ShoppingList.kt
@@ -1,8 +1,11 @@
 package com.jamiahus.myshoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem (
@@ -54,7 +58,6 @@ fun ShoppingListApp(){
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
-
         Button(
             onClick = { showDialog = true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
@@ -63,12 +66,12 @@ fun ShoppingListApp(){
         }
 
         LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
+            modifier = Modifier.fillMaxSize().padding(16.dp)
         ){
             items(sItems) {
-
+                ShoppingListItem(item = it, onEditClick = { /*TODO*/ }) {
+                    
+                }
             }
         }
     }
@@ -127,5 +130,20 @@ fun ShoppingListApp(){
                 }
             }
         )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row(modifier = Modifier.fillMaxWidth().padding(8.dp).border
+        (
+            border = BorderStroke(2.dp, Color.Black),
+            shape = RoundedCornerShape(20)
+        )) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
